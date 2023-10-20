419af819301eb19d6e1ac03bb935d482f48b6547
Seiji Akakabe <akakabeseiji0@gmail.com>
2023-10-20 06:14:52 -0400

    first commit

---
+++ music.ino
@@ -0,0 +1,32 @@
+#include <SD.h>                      // need to include the SD library
+//#define SD_ChipSelectPin 53  //example uses hardware SS pin 53 on Mega2560
+#define SD_ChipSelectPin 4  //using digital pin 4 on arduino nano 328, can use other pins
+#include <TMRpcm.h>           //  also need to include this library...
+#include <SPI.h>
+
+TMRpcm tmrpcm;   // create an object for use in this sketch
+
+unsigned long time = 0;
+
+void setup(){
+
+  tmrpcm.speakerPin = 9; //5,6,11 or 46 on Mega, 9 on Uno, Nano, etc
+  //Complimentary Output or Dual Speakers:
+  //pinMode(10,OUTPUT); Pin pairs: 9,10 Mega: 5-2,6-7,11-12,46-45 
+  
+  Serial.begin(115200);
+  pinMode(13,OUTPUT); //LED Connected to analog pin 0
+  if (!SD.begin(SD_ChipSelectPin)) {  // see if the card is present and can be initialized:
+    Serial.println("SD fail");  
+    return;   // don't do anything more if not
+
+  }
+  else{   
+    Serial.println("SD ok");   
+  }
+  tmrpcm.play("test.wav"); //the sound file "music" will play each time the arduino powers up, or is reset
+}
+
+
+
+void loop() {}
